853f46ddd293bed9ae7d7bd74a3d7f09a56d3693
Glitch (googhax) <none>
2019-11-16 02:47:13 +0000

    Another Update

--- commands/delmessage.js
+++ commands/delmessage.js
@@ -1,13 +1,15 @@
 exports.run = async (bot, message, args) => {
   //if (!message.member.hasPermission("ADMINISTRATOR")) return;
-  var messages = message.channel.messages.first(args);
-  messages.forEach(msg => msg.delete());
-  message.delete();
+  // var messages = message.channel.messages.first(Number(args)).forEach(msg => msg.delete());
+  // var smessages = await message.channel.fetchMessages({limit: args[0]});
+  // smessages.deleteAll();
+  message.channel.bulkDelete(Number(args));
+  //message.delete();
 };
 
 exports.help = {
   name: ["purge"],
-  desc: "(DEV ONLY) Test command",
+  desc: "Delete messages",
   admin: true,
   include: false
 };
